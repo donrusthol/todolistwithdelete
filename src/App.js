5e85fb6dc3f41af1c0cc5eea3a13debbb469dd3c
donrusthol <roope.rustholkarhu@gmail.com>
2023-02-17 15:20:36 +0200

    init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
 function App() {
+  const [desc, setDesc] = useState('');
+  const [date, setDate] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  const inputChanged = (e) => {
+    if (e.target.name === 'description') {
+      setDesc(e.target.value);
+    } else if (e.target.name === 'date') {
+      setDate(e.target.value);
+    }
+  }
+
+  const addTodo = (e) => {
+    e.preventDefault();
+    const newTodo = {description: desc, date: date};
+    setTodos([...todos, newTodo]);
+    setDesc('');
+    setDate('');
+  }
+
+  const deleteTodo = (index) => {
+    setTodos(todos.filter((todo, i) => i !== index));
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={addTodo}>
+        <label>Description: </label>
+        <input type = "text" name="description" value={desc} onChange={inputChanged} />
+        <label> Date: </label>
+        <input type="date" name="date" value={date} onChange={inputChanged} />
+        <input type = "submit" value="Add" />
+      </form>
+      <table>
+        <thead>
+          <tr>
+            <th>Description</th>
+            <th>Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            todos.map((todo, index) => 
+              <tr key = {index}>
+                <td>{todo.description}</td>
+                <td>{todo.date}</td>
+                <td><button onClick={() => deleteTodo(index)}>Delete</button></td>
+              </tr>
+            )
+          }
+        </tbody>
+      </table>
     </div>
   );
 }
